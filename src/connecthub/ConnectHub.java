2bcbe7bc5b0b54f5cb6a5200f3addb234be8260e
hala20110 <halaabdelsalam101@gmail.com>
2024-12-07 00:38:13 +0200

    conflict resolved

--- src/connecthub/ConnectHub.java
+++ src/connecthub/ConnectHub.java
@@ -15,7 +15,35 @@ public class ConnectHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+<<<<<<< HEAD
         WelcomePage w = new WelcomePage();
         w.setVisible(true);
     }   
+=======
+          AccountManagement admin = new AccountManagement();
+          //admin.signup("john3589@gmail.com","tony","11112222","25-6-2000",null,null,null);
+          admin.signup("waelnakhla38@gmail.com","kamal","13572468","25-6-2000",null,null,null);
+//        u1.addFriend(u2);
+//        u2.addFriend(u1);
+//        UsersDatabase database= new UsersDatabase();
+//        List<User> users = database.loadUsers();
+//        for(User k : users)
+//        {
+//            System.out.println(k.getUserId());
+//            if("User10005".equals(k.getUserId()))
+//            {
+//                System.out.println("here");
+//                k.setUsername("kamolya");
+//                System.out.println(k.getUsername());
+//                database.refresh(k);
+//            }
+//        }
+//        User u1 = new User("j389@gmail.com","dabdoob","1456","25-6-2000",null,null,null); 
+//        NewsFeedWindow nf= new NewsFeedWindow(u1);
+//        nf.setVisible(true);
+          LogInPage l = new LogInPage(admin);
+          l.setVisible(true);
+    }
+    
+>>>>>>> parent of 8aa4adc (newest update)
 }
